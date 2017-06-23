47a804c9f9a5096f13dabf7f923491b5c15eacee
yisus16 <ower@live.com>
2017-06-23 18:44:17 -0500

    Anchura Geocerca
    se aumento la anchura (distancia entre los 2 puntos) de la geocerca

--- direccion.php
+++ direccion.php
@@ -5,23 +5,23 @@
 	$origen  = ['lat' => $_POST['O_lat'],'lng' => $_POST['O_lng']];
 	$destino = ['lat' => $_POST['D_lat'],'lng' => $_POST['D_lng']];
 
-	$direccion =  \GeometryLibrary\SphericalUtil::computeHeading($origen, $destino); 
+	$direccion =  \GeometryLibrary\SphericalUtil::computeHeading($origen, $destino);
     
     if ($direccion >= -45 && $direccion <= 45) {
-		$left  =  \GeometryLibrary\SphericalUtil::computeOffset(['lat' => $origen['lat'], 'lng' => $origen['lng']], 40, $direccion -45);
-		$right =  \GeometryLibrary\SphericalUtil::computeOffset(['lat' => $origen['lat'], 'lng' => $origen['lng']], 40, $direccion +45);
+		$left  =  \GeometryLibrary\SphericalUtil::computeOffset(['lat' => $origen['lat'], 'lng' => $origen['lng']], 80, $direccion -45);
+		$right =  \GeometryLibrary\SphericalUtil::computeOffset(['lat' => $origen['lat'], 'lng' => $origen['lng']], 80, $direccion +45);
     } else if ($direccion >= 45 && $direccion <= 135) {
-		$left  =  \GeometryLibrary\SphericalUtil::computeOffset(['lat' => $origen['lat'], 'lng' => $origen['lng']], 40, $direccion -45);
-		$right =  \GeometryLibrary\SphericalUtil::computeOffset(['lat' => $origen['lat'], 'lng' => $origen['lng']], 40, $direccion +45);
+		$left  =  \GeometryLibrary\SphericalUtil::computeOffset(['lat' => $origen['lat'], 'lng' => $origen['lng']], 80, $direccion -90);
+		$right =  \GeometryLibrary\SphericalUtil::computeOffset(['lat' => $origen['lat'], 'lng' => $origen['lng']], 80, $direccion +90);
     } else if ($direccion >= 135  && $direccion <= 179.999) {
-		$left  =  \GeometryLibrary\SphericalUtil::computeOffset(['lat' => $origen['lat'], 'lng' => $origen['lng']], 40, $direccion -45);
-		$right =  \GeometryLibrary\SphericalUtil::computeOffset(['lat' => $origen['lat'], 'lng' => $origen['lng']], 40, $direccion +45);
+		$left  =  \GeometryLibrary\SphericalUtil::computeOffset(['lat' => $origen['lat'], 'lng' => $origen['lng']], 80, $direccion -45);
+		$right =  \GeometryLibrary\SphericalUtil::computeOffset(['lat' => $origen['lat'], 'lng' => $origen['lng']], 80, $direccion +45);
     } else if ($direccion <= -45  && $direccion >= -135) {
-		$left  =  \GeometryLibrary\SphericalUtil::computeOffset(['lat' => $origen['lat'], 'lng' => $origen['lng']], 40, $direccion -45);
-		$right =  \GeometryLibrary\SphericalUtil::computeOffset(['lat' => $origen['lat'], 'lng' => $origen['lng']], 40, $direccion +45);
+		$left  =  \GeometryLibrary\SphericalUtil::computeOffset(['lat' => $origen['lat'], 'lng' => $origen['lng']], 80, $direccion -90);
+		$right =  \GeometryLibrary\SphericalUtil::computeOffset(['lat' => $origen['lat'], 'lng' => $origen['lng']], 80, $direccion +90);
     } else if ($direccion <= -135  && $direccion >= -179.999) {
-		$left  =  \GeometryLibrary\SphericalUtil::computeOffset(['lat' => $origen['lat'], 'lng' => $origen['lng']], 40, $direccion -45);
-		$right =  \GeometryLibrary\SphericalUtil::computeOffset(['lat' => $origen['lat'], 'lng' => $origen['lng']], 40, $direccion +45);
+		$left  =  \GeometryLibrary\SphericalUtil::computeOffset(['lat' => $origen['lat'], 'lng' => $origen['lng']], 80, $direccion -45);
+		$right =  \GeometryLibrary\SphericalUtil::computeOffset(['lat' => $origen['lat'], 'lng' => $origen['lng']], 80, $direccion +45);
     } 
 
 	$geo[0] = $left;
